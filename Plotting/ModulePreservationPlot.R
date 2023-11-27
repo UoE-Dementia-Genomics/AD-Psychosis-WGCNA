2b83af3c28f37dc3b3400fb119c8aecc22e88bc0
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2023-11-27 13:29:27 +0000

    Create ModulePreservationPlot.R

---
+++ Plotting/ModulePreservationPlot.R
@@ -0,0 +1,37 @@
+module.preservation.plot <- function(MP.Data , Modules,title.medrank="",title.zsummary=""){
+  suppressMessages(library(WGCNA))
+  suppressMessages(library(ggplot2))
+  suppressMessages(library(ggrepel))
+  ref = 1 
+  test = 2 
+  
+  PlotData=cbind.data.frame(modColors = rownames(MP.Data$preservation$observed[[ref]][[test]]) ,
+                            moduleSizes = MP.Data$preservation$Z[[ref]][[test]][, 1],
+                            Zsummary = MP.Data$preservation$Z[[ref]][[test]][, 2],
+                            MedianRank=MP.Data$preservation$observed[[ref]][[test]][, 2])
+  PlotData <- PlotData[PlotData$modColors %in% Modules,]
+  
+  # plot median rank
+  p1 <- ggplot(PlotData, aes(moduleSizes, MedianRank,label=modColors,colour = modColors)) +         
+    geom_point(size = 10)+ 
+    scale_colour_manual(values=PlotData$modColors)+
+    geom_text_repel(colour = "black",max.overlaps = 10,size=4, vjust = 0, nudge_y = 0.5,hjust = -0.3,nudge_x = 0)+
+    #xlim(-50,1200)+
+    xlab("Module size")+ylab("Median rank")+ggtitle(title.medrank)+ theme_bw() +
+    theme(legend.position = "none")+scale_y_reverse() #(n.breaks =6,limits = c(12,0))
+  
+  # plot Z summary
+  p2 <- ggplot(PlotData, aes(moduleSizes, Zsummary,label=modColors,colour = modColors)) +         
+    geom_point(size = 10)+
+    scale_colour_manual(values=PlotData$modColors)+
+    geom_text_repel(colour = "black",max.overlaps = 10,size=4, vjust = 0, nudge_y = 0.5,hjust = -0.3,nudge_x = 0)+
+    xlab("Module size")+ylab("Z summary")+ggtitle(title.zsummary)+theme_bw() +
+    theme(legend.position = "none")+geom_hline(yintercept=0, color = "black")+
+    geom_hline(yintercept=5, linetype="dashed", color = "blue")+
+    geom_hline(yintercept=10, linetype="dashed", color = "red") +
+    scale_y_continuous(n.breaks = 8,limits = c(0,35))+
+    scale_x_continuous(n.breaks = 6,limits = c(-100,1200))
+  
+  return(list(Plot.Med.Rank=p1 , Plot.Z.Summary=p2))
+}
+
